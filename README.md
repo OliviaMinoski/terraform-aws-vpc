f4d406e9c1e5029aa6c0be6d63f7cffddc1576d1
OliviaMinoski <96315598+OliviaMinoski@users.noreply.github.com>
2022-03-21 21:08:36 -0400

    added public subnet

--- README.md
+++ README.md
@@ -4,8 +4,12 @@
 ```
 module "vpc" {
     source = "OliviaMinoski/vpc/aws"
+
     cidr_block = "10.0.0.0/16"
-    private_subnets_cidr = ["10.0.1.0/24"]
+
+    private_subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    public_subnets_cidr =  ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+   
     tags = {
         Name = "Dev"
     }
--- variable.tf
+++ variable.tf
@@ -1,8 +1,22 @@
-variable "cidr_block" {}
+variable "cidr_block" {
+  description = "Please provide a CIDR block to build a VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
+
 variable "tags" {
+    description = "Please provide a TAG to build VPC"
     type = map
 }
 
 variable "private_subnets_cidr" {
+    description = "A list of private subnets inside the VPC"
     type = list
+    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets_cidr" {
+    description = "A list of public subnets inside the VPC"
+    type = list
+    default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
